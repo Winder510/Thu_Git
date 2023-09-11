33d946643e2d6e4295257178f47367f4bf44b715
Winder510 <hoangphonghp04@gmail.com>
2023-09-12 00:54:34 +0700

    Them phep cong

---
+++ Phep_Tinh/PhepCong.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+float PhepCong(float a, float b)
+{
+	return a + b;
+}
+int main()
+{
+	int x=4, y=6;
+//	cin >> x >> y;
+	cout << "Phep cong " << PhepCong(x,y);
+
+}
