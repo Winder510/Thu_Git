48e25434d27714f8c9d2868000a28befb5c27a57
Winder510 <hoangphonghp04@gmail.com>
2023-09-12 16:51:21 +0700

    Them phep chia

--- Phep_Tinh/PhepCong.cpp
+++ Phep_Tinh/PhepCong.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+ #include<iostream>
 using namespace std;
 float PhepCong(float a, float b)
 {
 	return a + b;
 }
+float PhepChia(float a, float b)
+{
+	return a / b;
+}
 int main()
 {
-	int x=4, y=6;
+	float x=4, y=6;
 //	cin >> x >> y;
 	cout << "Phep cong " << PhepCong(x,y);
+	cout << "Phep Chia " << PhepChia(x, y);
 
 }
